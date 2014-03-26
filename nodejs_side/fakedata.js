edd6e45726fd063ba1f75f8ff745c67fd36e0ccf
Abhi Agarwal <me@abhiagarwal.com>
2014-03-26 16:14:27 -0400

    Removing extras

--- nodejs_side/fakedata.js
+++
@@ -1,9 +0,0 @@
-var serialPort = require("serialport");
-
-var sp = new serialPort.SerialPort("/dev/cu.usbmodem1421", {
-    baudrate: 9600,
-});
-
-sp.on("open", function (data) {
-  sp.write('E5F9D2B\r\n');
-});
